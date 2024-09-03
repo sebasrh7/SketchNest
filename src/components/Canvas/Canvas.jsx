1865b1fbd96a4465abdb32e0e85af170946c662d
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-09-03 17:37:42 -0500

    update styles

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useCanvas } from "../../contexts/CanvasContext";
 import Header from "./Header";
 
@@ -7,14 +7,25 @@ import "../../styles/Canvas.css";
 const Canvas = () => {
   const { canvasRef, startDrawing, draw, stopDrawing } = useCanvas();
 
+  // media query si la pantalla es menor a 900px
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 900px)");
+    const canvas = document.querySelector("canvas");
+    if (mediaQuery.matches) {
+      canvas.width = 300;
+      canvas.height = 500;
+    } else {
+      canvas.width = 600;
+      canvas.height = 400;
+    }
+  }, []);
+
   return (
     <section className="canvas">
       <Header />
       <div className="canvas-container">
         <canvas
           ref={canvasRef}
-          width={600}
-          height={400}
           onMouseDown={startDrawing}
           onMouseMove={draw}
           onMouseUp={stopDrawing}
