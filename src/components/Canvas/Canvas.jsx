c6c98f62c24085831c2f393f91c0c27ca8e8f38b
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-09-03 23:42:47 -0500

    remove unnecessary code

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useCanvas } from "../../contexts/CanvasContext";
 import Header from "./Header";
 
@@ -6,19 +6,6 @@ import "../../styles/Canvas.css";
 
 const Canvas = () => {
   const { canvasRef, startDrawing, draw, stopDrawing } = useCanvas();
-
-  // media query si la pantalla es menor a 900px
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 900px)");
-    const canvas = document.querySelector("canvas");
-    if (mediaQuery.matches) {
-      canvas.width = 300;
-      canvas.height = 500;
-    } else {
-      canvas.width = 600;
-      canvas.height = 400;
-    }
-  }, []);
 
   return (
     <section className="canvas">
