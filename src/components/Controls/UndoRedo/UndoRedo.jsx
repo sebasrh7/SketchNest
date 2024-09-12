228095f038840835234b43979173809d4856e49c
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-09-12 18:57:27 -0500

    Update dropdown styles

--- src/components/Controls/UndoRedo/UndoRedo.jsx
+++ src/components/Controls/UndoRedo/UndoRedo.jsx
@@ -16,7 +16,6 @@ const UndoRedo = ({ className }) => {
         disabled={undos?.length === 0}
         icon={<Back />}
       >
-        {undos.length}
       </Button>
       <Button
         className="redo-button"
@@ -24,7 +23,6 @@ const UndoRedo = ({ className }) => {
         disabled={redos?.length === 0}
         icon={<Forward />}
       >
-        {redos.length}
       </Button>
     </div>
   );
